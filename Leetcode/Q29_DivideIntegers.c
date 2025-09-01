84ccf6997b2feb94fc853d787d7fd07d555ffc57
OmkarSawant25 <omkarbro2525@gmail.com>
2025-09-01 15:23:25 +0800

    updated

--- Leetcode/Q29_DivideIntegers.c
+++ Leetcode/Q29_DivideIntegers.c
@@ -3,7 +3,7 @@
 
 int divide(int dividend, int divisor)
 {
-    if(dividend == INT_MIN && divisor == -1) return INT_MAX;
+    if(dividend == INT_MIN && divisor == -1) return 2147483647;
     return dividend/divisor;
 }
 
